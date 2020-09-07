2b88291da11d6893363f6b9d96351f54886630dc
Pipe98 <69595442+Pipe1998@users.noreply.github.com>
2020-09-07 11:50:22 -0300

    Add files via upload
    codigo parte 2

---
+++ 2.py
@@ -0,0 +1,6 @@
+#diccionario
+dic={"Felipe":[("programacion", 7.0),("fisica", 7.0)],"Pepito":[("programacion", 6.7),("fisica",5.9)],
+     "Maria":[("programacion", 7.0),("fisica", 6.6)], "Harold":[("programacion", 5.5),("fisica",7.0)]}
+
+print ("Pepito tiene nota "+str(dic["Pepito"][0][1])+" en "+dic["Pepito"][0][0]+"\n"+
+       "y tiene nota "+str(dic["Pepito"][1][1])+" en "+dic["Pepito"][1][0])
